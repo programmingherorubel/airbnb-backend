661b94e3605bbf06310b0e81d3f5e58ea0aa099e
programmingherorubel <programmingherorubel@gmail.com>
2023-09-03 14:15:36 +0600

    filter

--- Router/RoomsRouter.js
+++ Router/RoomsRouter.js
@@ -5,13 +5,13 @@ const CategorySchema = require('../Schema/RoomsSchema'); // Make sure to provide
 const Room = mongoose.model("Room", CategorySchema);
 const router = express.Router();
 
-router.post('/',async(req,res)=>{
-    const  singleRoom = Room(req.body) 
-    try{
+router.post('/', async (req, res) => {
+    const singleRoom = Room(req.body)
+    try {
         const addRoom = await singleRoom.save()
         res.status(200).json(addRoom)
     }
-    catch(error){
+    catch (error) {
         res.status(500).json({ "error": error.message });
     }
 })
@@ -20,20 +20,34 @@ router.post('/',async(req,res)=>{
 
 router.get('/', async (req, res) => {
     try {
-        const { category,price } = req.query; 
+        const { category, price } = req.query;
         
-        if (!category) {
+        if (!category && price === '100') {
             const rooms = await Room.find({});
             res.status(200).json(rooms);
         } else {
-            const rooms = await Room.find({ category:category });
-            console.log(rooms)
+            const filter = {}
+            if (category) {
+                filter.category = category;
+            }
+            
+            if (price) {
+                const numericPrice = parseFloat(price);
+                if (!isNaN(numericPrice)) { // Check if numericPrice is a valid number
+                    filter.price = { $gt: numericPrice };
+                } else {
+                    // Handle invalid price here, maybe return an error response
+                }
+            }
+            console.log(filter)
+            const rooms = await Room.find(filter);
             res.status(200).json(rooms);
         }
     } catch (error) {
         res.status(500).json({ "error": error.message });
     }
 });
+
 
 
 
