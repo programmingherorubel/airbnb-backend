9b3096ac18b5f69ce60b58fcc53bd52abd0643b1
programmingherorubel <programmingherorubel@gmail.com>
2023-09-03 18:00:02 +0600

    search working

--- Router/RoomsRouter.js
+++ Router/RoomsRouter.js
@@ -18,64 +18,43 @@ router.post('/', async (req, res) => {
 
 
 
-
-
-// router.get('/', async (req, res) => {
-//     try {
-//         const { category, price } = req.query;
-
-//         if (!category && price === '100') {
-//             const rooms = await Room.find({});
-//             res.status(200).json(rooms);
-//         } else {
-//             const filter = {}
-//             if (category) {
-//                 filter.category = category;
-//             }
-
-//             if (price) {
-//                 const numericPrice = parseFloat(price);
-//                 if (!isNaN(numericPrice)) { // Check if numericPrice is a valid number
-//                     filter.price = { $gt: numericPrice };
-//                 } 
-//             }
-//             console.log(filter)
-//             const rooms = await Room.find(filter);
-//             res.status(200).json(rooms);
-//         }
-//     } catch (error) {
-//         res.status(500).json({ "error": error.message });
-//     }
-// });
 router.get('/', async (req, res) => {
     try {
-        const { category, price } = req.query;
+        const { category, price, inputValue } = req.query;
 
-        if (!category && price === '100') {
-            const rooms = await Room.find({});
-            res.status(200).json(rooms);
-        } else {
-            const filter = {}
-            if (category) {
-                filter.category = category;
-            }
+       
+        const filter = {};
 
-            if (price) {
-                const numericPrice = parseFloat(price);
-                if (!isNaN(numericPrice)) { // Check if numericPrice is a valid number
-                    filter.price = { $gt: numericPrice };
-                } else {
-                    // Handle invalid price here, maybe return an error response
-                }
-            }
-            console.log(filter)
-            const rooms = await Room.find(filter);
-            res.status(200).json(rooms);
+        // category filter
+        if (category) {
+            filter.category = category;
         }
+
+        // price filter 
+        if (price) {
+            const numericPrice = parseFloat(price);
+            if (!isNaN(numericPrice)) {
+                filter.price = { $gt: numericPrice };
+            } else {
+                return res.status(400).json({ error: 'Invalid price value' });
+            }
+        }
+
+        // search Implement 
+        if (inputValue) {
+            // regular expression 
+            filter.location = { $regex: new RegExp(inputValue, 'i') };
+        }
+
+        // filter 
+        const rooms = await Room.find(filter);
+
+        res.status(200).json(rooms);
     } catch (error) {
-        res.status(500).json({ "error": error.message });
+        res.status(500).json({ error: error.message });
     }
 });
+
 
 
 
